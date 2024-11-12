1bb0efc44d63762faa5466cfc82160d196c5f7a4
Justin Neubert <neubert_justin@outlook.com>
2024-11-12 11:09:32 +0100

    feat: add Plugin model with fields for name, type, version, and creator

--- cmd/alertflow-runner/main.go
+++ cmd/alertflow-runner/main.go
@@ -13,7 +13,7 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-const version string = "0.12.2-beta"
+const version string = "0.12.3-beta"
 
 var (
 	configFile = kingpin.Flag("config", "Config File").Short('c').Default("config.yaml").String()
