9b91354937a57a2460fab8168bfd464e829aaca3
Justin Neubert <neubert_justin@outlook.com>
2025-03-17 21:43:12 +0100

    Update GetFlowData function to return multiple flow data types and refactor IncomingFlow structures

--- pkg/flows/get_data.go
+++ pkg/flows/get_data.go
@@ -6,15 +6,16 @@ import (
 	"net/http"
 	"time"
 
+	af_models "github.com/v1Flows/alertFlow/services/backend/pkg/models"
+	ef_models "github.com/v1Flows/exFlow/services/backend/pkg/models"
 	"github.com/v1Flows/runner/config"
 	"github.com/v1Flows/runner/pkg/models"
 	"github.com/v1Flows/runner/pkg/platform"
-	shared_models "github.com/v1Flows/shared-library/pkg/models"
 
 	log "github.com/sirupsen/logrus"
 )
 
-func GetFlowData(cfg config.Config, flowID string, targetPlatform string) (shared_models.Flows, error) {
+func GetFlowData(cfg config.Config, flowID string, targetPlatform string) (exFlow ef_models.Flows, alertFlow af_models.Flows, err error) {
 	client := http.Client{
 		Timeout: 10 * time.Second,
 		Transport: &http.Transport{
@@ -28,29 +29,33 @@ func GetFlowData(cfg config.Config, flowID string, targetPlatform string) (share
 	req, err := http.NewRequest("GET", parsedUrl, nil)
 	if err != nil {
 		log.Errorf("Failed to create request: %v", err)
-		return shared_models.Flows{}, err
+		return ef_models.Flows{}, af_models.Flows{}, err
 	}
 	req.Header.Set("Authorization", apiKey)
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Error(err)
-		return shared_models.Flows{}, err
+		return ef_models.Flows{}, af_models.Flows{}, err
 	}
 
 	if resp.StatusCode != 200 {
 		log.Errorf("Failed to get flow data from %s API: %s", targetPlatform, url)
 		err = fmt.Errorf("failed to get flow data from %s API: %s", targetPlatform, url)
-		return shared_models.Flows{}, err
+		return ef_models.Flows{}, af_models.Flows{}, err
 	}
 
 	log.Debugf("Flow data received from %s API: %s", targetPlatform, url)
 
-	var flow models.IncomingFlow
-	err = json.NewDecoder(resp.Body).Decode(&flow)
-	if err != nil {
+	var afFlow models.IncomingAfFlow
+	aferr := json.NewDecoder(resp.Body).Decode(&afFlow)
+
+	var efFlow models.IncomingEfFlow
+	eferr := json.NewDecoder(resp.Body).Decode(&efFlow)
+
+	if aferr != nil && eferr != nil {
 		log.Fatal(err)
-		return shared_models.Flows{}, err
+		return ef_models.Flows{}, af_models.Flows{}, err
 	}
 
-	return flow.FlowData, nil
+	return efFlow.FlowData, afFlow.FlowData, nil
 }
--- pkg/models/flows.go
+++ pkg/models/flows.go
@@ -1,9 +1,14 @@
 package models
 
 import (
-	shared_models "github.com/v1Flows/shared-library/pkg/models"
+	af_models "github.com/v1Flows/alertFlow/services/backend/pkg/models"
+	ef_models "github.com/v1Flows/exFlow/services/backend/pkg/models"
 )
 
-type IncomingFlow struct {
-	FlowData shared_models.Flows `json:"flow"`
+type IncomingAfFlow struct {
+	FlowData af_models.Flows `json:"flow"`
+}
+
+type IncomingEfFlow struct {
+	FlowData ef_models.Flows `json:"flow"`
 }
