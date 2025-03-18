6b05c1bd7844013452cc09d43730de8e6035f073
Justin Neubert <neubert_justin@outlook.com>
2025-03-18 08:35:23 +0100

    Refactor GetFlowData function to return raw byte data and improve error handling for API responses

--- pkg/flows/get_data.go
+++ pkg/flows/get_data.go
@@ -1,22 +1,18 @@
 package flows
 
 import (
-	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"time"
 
-	af_models "github.com/v1Flows/alertFlow/services/backend/pkg/models"
-	ef_models "github.com/v1Flows/exFlow/services/backend/pkg/models"
 	"github.com/v1Flows/runner/config"
-	"github.com/v1Flows/runner/pkg/models"
 	"github.com/v1Flows/runner/pkg/platform"
-	shared_models "github.com/v1Flows/shared-library/pkg/models"
 
 	log "github.com/sirupsen/logrus"
 )
 
-func GetFlowData(cfg config.Config, flowID string, targetPlatform string) (exFlow ef_models.Flows, alertFlow af_models.Flows, sharedFlow shared_models.Flows, err error) {
+func GetFlowData(cfg config.Config, flowID string, targetPlatform string) (bytes []byte, err error) {
 	client := http.Client{
 		Timeout: 10 * time.Second,
 		Transport: &http.Transport{
@@ -30,56 +26,28 @@ func GetFlowData(cfg config.Config, flowID string, targetPlatform string) (exFlo
 	req, err := http.NewRequest("GET", parsedUrl, nil)
 	if err != nil {
 		log.Errorf("Failed to create request: %v", err)
-		return ef_models.Flows{}, af_models.Flows{}, shared_models.Flows{}, err
+		return nil, err
 	}
 	req.Header.Set("Authorization", apiKey)
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Error(err)
-		return ef_models.Flows{}, af_models.Flows{}, shared_models.Flows{}, err
+		return nil, err
 	}
 
 	if resp.StatusCode != 200 {
 		log.Errorf("Failed to get flow data from %s API: %s", targetPlatform, url)
 		err = fmt.Errorf("failed to get flow data from %s API: %s", targetPlatform, url)
-		return ef_models.Flows{}, af_models.Flows{}, shared_models.Flows{}, err
+		return nil, err
 	}
 
 	log.Debugf("Flow data received from %s API: %s", targetPlatform, url)
 
-	if targetPlatform == "alertflow" {
-		var flow models.IncomingAfFlow
-		err := json.NewDecoder(resp.Body).Decode(&flow)
-		if err != nil {
-			log.Fatal(err)
-			return ef_models.Flows{}, af_models.Flows{}, shared_models.Flows{}, err
-		}
-
-		var sharedFlow models.IncomingSharedFlow
-		err = json.NewDecoder(resp.Body).Decode(&sharedFlow)
-		if err != nil {
-			log.Fatal(err)
-			return ef_models.Flows{}, af_models.Flows{}, shared_models.Flows{}, err
-		}
-
-		return ef_models.Flows{}, flow.FlowData, sharedFlow.FlowData, nil
-	} else if targetPlatform == "exflow" {
-		var flow models.IncomingEfFlow
-		err := json.NewDecoder(resp.Body).Decode(&flow)
-		if err != nil {
-			log.Fatal(err)
-			return ef_models.Flows{}, af_models.Flows{}, shared_models.Flows{}, err
-		}
-
-		var sharedFlow models.IncomingSharedFlow
-		err = json.NewDecoder(resp.Body).Decode(&sharedFlow)
-		if err != nil {
-			log.Fatal(err)
-			return ef_models.Flows{}, af_models.Flows{}, shared_models.Flows{}, err
-		}
-
-		return flow.FlowData, af_models.Flows{}, sharedFlow.FlowData, nil
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
 	}
 
-	return ef_models.Flows{}, af_models.Flows{}, shared_models.Flows{}, fmt.Errorf("unknown target platform: %s", targetPlatform)
+	return body, nil
 }
