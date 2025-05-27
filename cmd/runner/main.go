dad660809a755a12e83e598a43fe89235847dba7
Justin Neubert <neubert_justin@outlook.com>
2025-05-27 10:22:48 +0200

    fix: correct log message for ExFlow registration

--- cmd/runner/main.go
+++ cmd/runner/main.go
@@ -89,7 +89,7 @@ func main() {
 	}
 
 	if cfg.ExFlow.Enabled {
-		log.Info("Register at ExFlow...")
+		log.Info("Register at exFlow...")
 		runner.RegisterAtAPI("exflow", version, modelPlugins, actions, nil)
 		go runner.SendHeartbeat("exflow")
 		Init("exflow", cfg, router, actions, endpointPlugins, loadedPlugins)
