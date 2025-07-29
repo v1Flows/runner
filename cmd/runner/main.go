adddfd42c0bd2dcfc323c6bd2064c4cfc67f36ee
Justin Neubert <neubert_justin@outlook.com>
2025-07-29 14:56:41 +0200

    Bump version to 1.3.0

--- cmd/runner/main.go
+++ cmd/runner/main.go
@@ -23,7 +23,7 @@ import (
 
 var (
 	log        = logrus.New()
-	version    = "1.2.4"
+	version    = "1.3.0"
 	configFile = kingpin.Flag("config", "Path to configuration file").Short('c').String()
 )
 
