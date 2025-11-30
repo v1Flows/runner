863d0baa6adb38e3552fc7c0e70373c5c0801d5e
Justin Neubert <neubert_justin@outlook.com>
2025-11-30 13:21:13 +0100

    Bump version to 2.0.0-beta.2

--- cmd/runner/main.go
+++ cmd/runner/main.go
@@ -23,7 +23,7 @@ import (
 
 var (
 	log        = logrus.New()
-	version    = "2.0.0-beta.1"
+	version    = "2.0.0-beta.2"
 	configFile = kingpin.Flag("config", "Path to configuration file").Short('c').String()
 )
 
